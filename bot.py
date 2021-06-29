141293c6c21a2ee281a4c202f812c1954961a8f7
JacobStacy <35983856+JacobStacy@users.noreply.github.com>
2021-06-29 16:45:41 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,9 @@
 # bot.py
 
 import asyncio
-from discord import client, message
 from discord.ext.commands import Bot
 from datetime import datetime
 import discord
-import threading
 
 
 bot = Bot(command_prefix='!')
@@ -20,16 +18,7 @@ async def on_ready(): #Sets up bot and reports to console
 
 	await checkTime()
 	
-
-
-@bot.command(name='server')
-async def fetchServerInfo(context):
-	guild = context.guild
 	
-	await context.send(f'Server Name: {guild.name}')
-	await context.send(f'Server Size: {len(guild.members)}')
-	await context.send(f'Server Name: {guild.owner.display_name}')
-
 async def sendBong(hour):
 	for guild in bot.guilds:
 			if hour < 12:
@@ -49,10 +38,6 @@ async def checkTime():
 		if(current_time[3:] == '00:00'):  # check if matches with the desired time
 			await sendBong(hour)
 		await asyncio.sleep(1) # task runs every 60 seconds
-	
-
-		
-
 
 
 bot.run(TOKEN)
