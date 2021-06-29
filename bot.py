78d9c2134317416edf7967a1680ca1d98097dec8
JacobStacy <35983856+JacobStacy@users.noreply.github.com>
2021-06-29 19:48:19 -0400

    Full Bonging

--- bot.py
+++ bot.py
@@ -1,9 +1,11 @@
 # bot.py
 
 import asyncio
+from discord import channel
 from discord.ext.commands import Bot
 from datetime import datetime
 import discord
+from discord.player import FFmpegPCMAudio
 
 
 bot = Bot(command_prefix='!')
@@ -18,13 +20,46 @@ async def on_ready(): #Sets up bot and reports to console
 
 	await checkTime()
 	
-	
+
 async def sendBong(hour):
 	for guild in bot.guilds:
+
+		prev = None
+		prev_size = 0
+		
+		for channel in guild.voice_channels:
+			if len(channel.members) >= prev_size:
+				prev = channel
+				prev_size = len(channel.members)
+			
+		if prev_size > 0 :
+			vc = await prev.connect()
+		
+			vc.play(FFmpegPCMAudio("Startup.mp3",))
+			await asyncio.sleep(21.994) # length of auto clip
+
 			if hour < 12:
-				await guild.text_channels[0].send("Bong " * hour)
+				for i in range((hour - 1)):
+					vc.play(FFmpegPCMAudio("Bong.mp3"))
+					await asyncio.sleep(2.718) # length of auto clip
 			else:
-				await guild.text_channels[0].send("Bong " * (hour-12))
+				for i in range((hour - 13)):
+					vc.play(FFmpegPCMAudio("Bong.mp3"))
+					await asyncio.sleep(2.718) # length of auto clip
+		
+			vc.play(FFmpegPCMAudio("LastBong.mp3"))
+			await asyncio.sleep(18.574) # length of auto clip
+			
+			await vc.disconnect()
+
+			await bot.get_channel(859570392402493461).send("Bonged in '" + guild.name  + "' in the channel '" + channel.name + "' " + datetime.now().strftime("%H:%M:%S"))
+
+		else:
+			if hour < 12:
+				await guild.text_channels[-1].send("Bong " * hour)
+			else:
+				await guild.text_channels[-1].send("Bong " * (hour-12))
+
 
 
 async def checkTime():
@@ -38,6 +73,13 @@ async def checkTime():
 		if(current_time[3:] == '00:00'):  # check if matches with the desired time
 			await sendBong(hour)
 		await asyncio.sleep(1) # task runs every 60 seconds
+
+
+@bot.command(name='bong')
+async def manBong(ctx):
+	now = datetime.now()
+	hour = int(now.strftime("%H"))
+	await sendBong(hour)
 
 
 bot.run(TOKEN)
